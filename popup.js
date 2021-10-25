c161ef1b5afa76c19f485577e306ec7f09794e1d
Wilbur Rapieng <wrapieng@umich.edu>
2021-10-25 14:54:49 -0400

    testing with styling and close button functions

--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
+
+
 window.addEventListener('load', (event) => {
+    var closeButton = document.getElementById("closeButton");
     document.getElementById("inputText").addEventListener("change", event =>{
     document.getElementById("headingText").innerHTML = event.target.value;
+    
+    });
+
+    closeButton.addEventListener("click", async () =>{
+        window.close();
+        
     });
 });
+
+
+
 

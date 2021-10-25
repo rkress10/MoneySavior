26e96d69ee7d6dc7aa0a7fa52b2d0eea65463404
rkress <rkress@umich.edu>
2021-10-24 21:58:27 -0400

    starter files

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function GetCurrentTab() {
     chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
         var tab = tabs[0];
         chrome.storage.sync.get({'google_visits': 1}, function(data) {
-            alert('baby');
+            alert('updating number of visits');
         });
         chrome.storage.sync.set({'google_visits': chrome.storage.sync.get({'google_visits': 1}, function(data) {})})
         alert(tab.url);

a7d288c9c31af15b710c23b5c6739d542eefa964
rkress <rkress@umich.edu>
2021-10-26 19:54:55 -0400

    background.js

--- background.js
+++ background.js
@@ -1,32 +1,39 @@
-hostnames = ["www.google.com", "www.hulu.com", "www.netflix.com"];
-
 function GetCurrentTab() {
     chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
         let taburl = new URL(tabs[0].url);
-        if (hostnames.includes(taburl.hostname)) {
-            switch (taburl.hostname) {
-                case 'www.netflix.com':
-                    alert("Streaming website: Netflix");
-                    chrome.storage.sync.get({"www.netflix.com": 0}, function(data) {
-                        chrome.storage.sync.set({"www.netflix.com": data["www.netflix.com"] + 1}, function() {});
-                    });
-                    break;
-                case 'www.hulu.com':
-                    alert("Streaming website: Hulu");
-                    chrome.storage.sync.get({"www.hulu.com": 0}, function(data) {
-                        chrome.storage.sync.set({"www.hulu.com": data["www.hulu.com"] + 1}, function() {});
-                    });
-                    break;
-                case 'www.google.com':
-                    alert("Streaming website: Google");
-                    chrome.storage.sync.get({"www.google.com": 0}, function(data) {
-                        chrome.storage.sync.set({"www.google.com": data["www.google.com"] + 1}, function() {});
-                    });
-                    break;
-                default:
-                    alert('Somethings Wrong');
-            }
-            }
+        chrome.storage.sync.get({"hostnames":[]}, function(data) {
+            if (data["hostnames"].includes(taburl.hostname)) {
+                switch (taburl.hostname) {
+                    case 'www.netflix.com':
+                        alert("Streaming website: Netflix");
+                        chrome.storage.sync.get({"www.netflix.com": 0}, function(data) {
+                            chrome.storage.sync.set({"www.netflix.com": data["www.netflix.com"] + 1}, function() {});
+                        });
+                        break;
+                    case 'www.hulu.com':
+                        alert("Streaming website: Hulu");
+                        chrome.storage.sync.get({"www.hulu.com": 0}, function(data) {
+                            chrome.storage.sync.set({"www.hulu.com": data["www.hulu.com"] + 1}, function() {});
+                        });
+                        break;
+                    case 'www.peacocktv.com':
+                        alert("Streaming website: peacock");
+                        chrome.storage.sync.get({"www.peacocktv.com": 0}, function(data) {
+                            chrome.storage.sync.set({"www.peacocktv.com": data["www.peacocktv.com"] + 1}, function() {});
+                        });
+                        break;
+                    case 'www.hbomax.com':
+                        alert("Streaming website: hbo max");
+                        chrome.storage.sync.get({"www.hbomax.com": 0}, function(data) {
+                            chrome.storage.sync.set({"www.hbomax.com": data["www.hbomax.com"] + 1}, function() {});
+                        });
+                        break;
+                    default:
+                        alert('Somethings Wrong');
+                }
+                }
+        })
+        
         }
     )
 }

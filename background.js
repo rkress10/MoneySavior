6248d76373e527e7f58fa6d3fcae8e15eb8619c3
rkress <rkress@umich.edu>
2021-10-25 20:26:49 -0400

    tracks nivits to 3 websites

--- background.js
+++ background.js
@@ -1,17 +1,38 @@
+hostnames = ["www.google.com", "www.hulu.com", "www.netflix.com"];
+
 function GetCurrentTab() {
     chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
-        var tab = tabs[0];
-        chrome.storage.sync.get({'google_visits': 1}, function(data) {
-            alert('updating number of visits');
-        });
-        chrome.storage.sync.set({'google_visits': chrome.storage.sync.get({'google_visits': 1}, function(data) {})})
-        alert(tab.url);
-        return tab.url;
-    })
+        let taburl = new URL(tabs[0].url);
+        if (hostnames.includes(taburl.hostname)) {
+            switch (taburl.hostname) {
+                case 'www.netflix.com':
+                    alert("Streaming website: Netflix");
+                    chrome.storage.sync.get({"www.netflix.com": 0}, function(data) {
+                        chrome.storage.sync.set({"www.netflix.com": data["www.netflix.com"] + 1}, function() {});
+                    });
+                    break;
+                case 'www.hulu.com':
+                    alert("Streaming website: Hulu");
+                    chrome.storage.sync.get({"www.hulu.com": 0}, function(data) {
+                        chrome.storage.sync.set({"www.hulu.com": data["www.hulu.com"] + 1}, function() {});
+                    });
+                    break;
+                case 'www.google.com':
+                    alert("Streaming website: Google");
+                    chrome.storage.sync.get({"www.google.com": 0}, function(data) {
+                        chrome.storage.sync.set({"www.google.com": data["www.google.com"] + 1}, function() {});
+                    });
+                    break;
+                default:
+                    alert('Somethings Wrong');
+            }
+            }
+        }
+    )
 }
 
 chrome.tabs.onUpdated.addListener (function (tabID, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-        let url = GetCurrentTab();
+        GetCurrentTab();
     }
 })
--- popup.js
+++ popup.js
@@ -1,18 +1,63 @@
-
-
 window.addEventListener('load', (event) => {
-    var closeButton = document.getElementById("closeButton");
-    document.getElementById("inputText").addEventListener("change", event =>{
-    document.getElementById("headingText").innerHTML = event.target.value;
     
-    });
-
-    closeButton.addEventListener("click", async () =>{
-        window.close();
-        
-    });
 });
 
+window.onload=function() {
+    here();
+    here2();
+    here3();
+    deleteButton();
+    closeButton();
+}
 
 
+function here() {
+    document.getElementById('showDataGoog').addEventListener("click",
+    function() {
+        chrome.storage.sync.get({"www.google.com": 0}, function(data) {
+            console.log(data);
+            document.getElementById('showDataGoogle').innerHTML = "Number of Google Visits: " + data["www.google.com"];
+        });
+    });
+}
 
+function here2() {
+    document.getElementById('showDataNetf').addEventListener("click",
+    function() {
+        chrome.storage.sync.get({"www.netflix.com": 0}, function(data) {
+            console.log(data);
+            document.getElementById('showDataNetflix').innerHTML = "Number of Netflix Visits: " + data["www.netflix.com"];
+        });
+    });
+}
+
+function here3() {
+    document.getElementById('showDataHulu').addEventListener("click",
+    function() {
+        chrome.storage.sync.get({"www.hulu.com": 0}, function(data) {
+            console.log(data);
+            document.getElementById('showDataHuluText').innerHTML = "Number of Hulu Visits: " + data["www.hulu.com"];
+        });
+    });
+}
+
+// deletes data from storage
+// TODO: add preventative measures to make sure user doesn't accidentally hit the button
+function deleteButton() {
+    document.getElementById('deleteData').addEventListener('click',
+    function () {
+        chrome.storage.sync.remove(['google_visits', "www.google.com"], function() {
+            alert("Deleted Stored Data");
+        })
+    }
+    );
+}
+
+// adds functionality to close button
+function closeButton() {
+    document.getElementById('closeButton').addEventListener("click",
+    function() {
+        window.close();
+    }
+    );
+}
